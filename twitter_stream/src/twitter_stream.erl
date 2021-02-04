e18e65e9962352cc4551f22cc74547ef92a16354
galathinius <anisoara.ionela@gmail.com>
2021-02-04 21:09:02 +0200

    ceva lucreaza

--- twitter_stream/src/twitter_stream.erl
+++ twitter_stream/src/twitter_stream.erl
@@ -10,11 +10,29 @@ main() ->
     io:format("~p~n", [Response]),
     shotgun:close(Conn).
 
+% twitter_stream:tweets1().
+
 tweets1() ->
     % not tested
-    {ok, Conn} = shotgun:open("locahost", 4000),
+    {ok, Conn} = shotgun:open("localhost", 4000),
     Options = #{async => true, async_mode => sse},
     {ok, Ref} = shotgun:get(Conn, "/tweets/1", #{},
 			    Options),
+    wait(1),
     Events = shotgun:events(Conn),
-    shotgun:events(Conn).
+    process_events(Events),
+    % io:format("~p~n", [Events]),
+    % shotgun:events(Conn).
+    shotgun:close(Conn).
+
+process_events(Events) ->
+    [Head | Tail] = Events,
+    one_event(Head),
+    process_events(Tail).
+
+one_event(Event) ->
+    {_, _, Message} = Event,
+    Text = shotgun:parse_event(Message),
+    io:format("~p~n", [Text]).
+
+wait(Sec) -> receive  after 10 * Sec -> ok end.

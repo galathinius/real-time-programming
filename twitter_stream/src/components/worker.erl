499278250d4eb795af99d1c0860dc0e2771b97eb
galathinius <anisoara.ionela@gmail.com>
2021-02-21 04:26:28 +0200

    and thinking -> random wait in worker

--- twitter_stream/src/components/worker.erl
+++ twitter_stream/src/components/worker.erl
@@ -13,37 +13,36 @@ handle_info(Tweet, State) ->
     one_event(Tweet), {noreply, State}.
 
 one_event(Event) ->
+    thinking(),
     Text = shotgun:parse_event(Event),
     #{data := Data} = Text,
     Isjson = jsx:is_json(Data),
-    if Isjson == true -> ok;
-       % process_map(Data);
+    if Isjson == true -> process_map(Data);
        true -> detect_panic(Data)
     end.
 
 detect_panic(Map) ->
-    % io:format("~p~p~n", ["one ", Map]),
     TheMap = unicode:characters_to_list(Map, utf8),
-    % io:format("~p~p~n", ["two ", TheMap]),
     Index = string:str(TheMap, "panic"),
     if Index > 0 ->
 	   io:format("~p~p~n", ["paniking ", self()]), exit(undef);
-       % process_map(Data);
        true -> ok
     end.
 
-    % #{<<"message">> := Message} = TheMap,
-
 process_map(Amap) ->
-    % io:format("~p~n", ["\ngot something\n"]),
     TheMap = jsx:decode(Amap),
     #{<<"message">> :=
 	  #{<<"tweet">> := #{<<"text">> := Text}}} =
 	TheMap,
     NotBin = unicode:characters_to_list(Text, utf8),
     Low = string:lowercase(NotBin),
-    Chunks = string:tokens(Low, [$\s]).
+    Chunks = string:tokens(Low, [$\s]),
+    io:format("~p~p~n", [self(), Chunks]).
 
-    % io:format("~p~p~n", [self(), Chunks]).
+thinking() ->
+    Ra = rand:uniform(),
+    Int = round(Ra * 100),
+    wait(Int),
+    ok.
 
-wait(Sec) -> receive  after 100 * Sec -> ok end.
+wait(Unit) -> receive  after 10 * Unit -> ok end.

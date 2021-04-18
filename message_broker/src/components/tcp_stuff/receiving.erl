d7dc6cc84643e9a893b552c4dda928ca9cae1c50
galathinius <anisoara.ionela@gmail.com>
2021-04-19 01:38:04 +0300

    message parsing

--- message_broker/src/components/tcp_stuff/receiving.erl
+++ message_broker/src/components/tcp_stuff/receiving.erl
@@ -6,6 +6,7 @@
 process_message(#{<<"request">> := <<"publish">>,
                   <<"topics">> := Topics},
                 ListenSocket, SendSock) ->
+    io:format("got a publisher req ~p~n", [self()]),
     % register topics
     table:add_topics(Topics),
     % send ok
@@ -16,8 +17,9 @@ process_message(#{<<"request">> := <<"publish">>,
     listening_socket:handle_connection(ListenSocket);
 %
 % topic request
-process_message(#{<<"request">> := <<"topics">>},
+process_message({[{<<"request">>, <<"topics">>}]},
                 ListenSocket, SendSock) ->
+    io:format("got a topic req ~p~n", [self()]),
     % get topics
     Topics = table:get_topic_list(),
     % send topics
@@ -26,21 +28,26 @@ process_message(#{<<"request">> := <<"topics">>},
     listening_socket:handle_connection(ListenSocket);
 %
 % new subscribe request
-process_message(#{<<"request">> := <<"subscribe">>,
-                  <<"topics">> := Topics},
+process_message({[{<<"topics">>, Topics},
+                  {<<"request">>, <<"subscribe">>}]},
                 ListenSocket, SendSock) ->
+    io:format("got a new sub req ~p for ~p ~n",
+              [self(), Topics]),
     % create id
     Id = uuid:to_string(uuid:uuid1()),
-    tables:add_sub(Id),
-    process_message(#{<<"request">> => <<"subscribe">>,
-                      <<"id">> => <<Id>>, <<"topics">> => Topics},
+    table:add_sub(Id),
+    process_message({[{<<"id">>, Id},
+                      {<<"topics">>, Topics},
+                      {<<"request">>, <<"subscribe">>}]},
                     ListenSocket,
                     SendSock);
 %
 % subscribe request from already registered client
-process_message(#{<<"request">> := <<"subscribe">>,
-                  <<"id">> := <<Id>>, <<"topics">> := Topics},
+process_message({[{<<"id">>, Id},
+                  {<<"topics">>, Topics},
+                  {<<"request">>, <<"subscribe">>}]},
                 ListenSocket, SendSock) ->
+    io:format("got a old sub req ~p~n", [self()]),
     % check id
     table:check_sub_E(Id),
     % send id as confirmation
@@ -55,9 +62,11 @@ process_message(#{<<"request">> := <<"subscribe">>,
     listening_socket:accept(ListenSocket);
 %
 % unsubscribe request
-process_message(#{<<"request">> := <<"unsubscribe">>,
-                  <<"id">> := <<Id>>, <<"topics">> := Topics},
+process_message({[{<<"id">>, Id},
+                  {<<"topics">>, Topics},
+                  {<<"request">>, <<"unsubscribe">>}]},
                 ListenSocket, SendSock) ->
+    io:format("got a unsub req ~p~n", [self()]),
     % check id
     table:check_sub_E(Id),
     % send id as confirmation
@@ -69,4 +78,10 @@ process_message(#{<<"request">> := <<"unsubscribe">>,
     table:remove_sub_from_topics(Id, TopicsToSub),
     % wait for another connection
     % check for bug, if conn is not closed
-    listening_socket:accept(ListenSocket).
+    listening_socket:accept(ListenSocket);
+process_message({[{<<"data">>, Data},
+                  {<<"topics">>, Topics}]},
+                ListenSocket, SendSock) ->
+    io:format("got a dat mess ~p~n", [self()]),
+    % idk, send along
+    ok.
